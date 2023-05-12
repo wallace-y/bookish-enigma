1494bec8b2f6420570e0387ad65942eeece975e6
wallace-y <cameronwfinney@gmail.com>
2023-05-12 15:29:03 +0100

    adding patch endpoint for comments/:comment_id

--- models/models.js
+++ models/models.js
@@ -143,6 +143,32 @@ function selectUserByUsername(username) {
     });
 }
 
+function updateCommentById(comment_id, update) {
+  return connection
+    .query(`SELECT votes FROM comments WHERE comment_id = $1`, [comment_id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Resource not found." });
+      }
+      let newVotes = result.rows[0].votes + update.inc_votes;
+      if (newVotes < 0) {
+        return 0;
+      } else {
+        return newVotes;
+      }
+    })
+    .then((newVotes) => {
+      return connection
+        .query(
+          `UPDATE comments SET votes = $1 WHERE comment_id = $2 RETURNING *;`,
+          [newVotes, comment_id]
+        )
+        .then(({ rows }) => {
+          return rows[0];
+        });
+    });
+}
+
 module.exports = {
   selectReviewById,
   selectReviews,
@@ -151,4 +177,5 @@ module.exports = {
   selectComments,
   removeComment,
   selectUserByUsername,
+  updateCommentById,
 };
